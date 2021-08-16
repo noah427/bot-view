9c5becf570468df4d20d7e0a79a899fe5fb36cc1
naoh427 <noahafeinberg@icloud.com>
2021-08-16 03:22:59 -0400

    basic working version

--- main.go
+++ main.go
@@ -16,13 +16,13 @@ var fs embed.FS
 
 func main() {
 	godotenv.Load()
-	ln, _ := net.Listen("tcp", "127.0.0.1:3000")
+	ln, _ := net.Listen("tcp", "127.0.0.1:4242")
 	go http.Serve(ln, http.FileServer(http.FS(fs)))
 
 	ui, _ := lorca.New("", "", 480, 320)
 	defer ui.Close()
 
-	ui.Bind("guilds", func() []*discordgo.UserGuild {
+	ui.Bind("guildsGO", func() []*discordgo.UserGuild {
 		guilds, err := Client.UserGuilds(100, "", "")
 		if err != nil {
 			fmt.Println(err)
@@ -30,9 +30,37 @@ func main() {
 		return guilds
 	})
 
+	ui.Bind("channelsGO", func(id string) []*discordgo.Channel {
+		channels, err := Client.GuildChannels(id)
+		if err != nil {
+			fmt.Println(err)
+		}
+		return channels
+	})
+
+	ui.Bind("messagesGO", func(id string) []*discordgo.Message {
+		messages, err := Client.ChannelMessages(id, 25, "", "", "")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		for i, j := 0, len(messages)-1; i < j; i, j = i+1, j-1 {
+			messages[i], messages[j] = messages[j], messages[i]
+		}
+
+		return messages
+	})
+
+	ui.Bind("sendGO", func( id,content string) {
+		_,err := Client.ChannelMessageSend(id, content)
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+
 	loadDiscord()
 
-	ui.Load("http://127.0.0.1:3000/public")
+	ui.Load("http://127.0.0.1:4242/public")
 
 	<-ui.Done()
 	Client.Close()
--- public/index.js
+++ public/index.js
@@ -1,3 +1,88 @@
-(async ()=>{
-    console.log(await guilds())
+var messageInput = document.getElementById('messageInput')
+var state // channel id
+var refresh // reload messages
+
+class Message {
+  constructor (go) {
+    this.ID = go.id
+    this.Content = go.content
+    this.Author = go.author
+    this.Element = this.Div()
+  }
+
+  Div () {
+    var e = document.createElement('div')
+    e.classList.add('message')
+    e.innerHTML = `${this.Author.username}:${this.Content}`
+    return e
+  }
+}
+
+class Channel {
+  constructor (go) {
+    this.ID = go.id
+    this.Name = go.name
+    this.Type = go.type
+    this.Element = this.Div()
+  }
+
+  Div () {
+    var e = document.createElement('div')
+    e.classList.add('channel')
+    e.innerHTML = this.Name
+    e.onclick = this.loadMessages.bind(this)
+    return e
+  }
+
+  async loadMessages () {
+    state = this.ID
+    refresh = this.loadMessages.bind(this)
+    document.getElementById('messages').innerHTML = ''
+    var messages = await messagesGO(this.ID)
+    for (let m of messages) {
+      var message = new Message(m)
+      document.getElementById('messages').append(message.Element)
+    }
+  }
+}
+class Guild {
+  constructor (go) {
+    this.ID = go.id
+    this.Name = go.name
+    this.Icon = `https://cdn.discordapp.com/icons/${this.ID}/${go.icon}.png`
+    this.Element = this.image()
+  }
+
+  image () {
+    var e = document.createElement('img')
+    e.classList.add('icon')
+    e.src = this.Icon
+    e.onclick = this.loadChannels.bind(this)
+    return e
+  }
+
+  async loadChannels () {
+    document.getElementById('channels').innerHTML = ''
+    var channels = await channelsGO(this.ID)
+    for (let c of channels) {
+      var channel = new Channel(c)
+      if (c.type == 'topic') continue
+      document.getElementById('channels').append(channel.Element)
+    }
+  }
+}
+
+document.onkeypress = function (e) {
+  if (e.key == 'Enter') {
+    sendGO(state, messageInput.value)
+    refresh()
+  }
+}
+;(async () => {
+  var guilds = await guildsGO()
+  for (const g of guilds) {
+    var guild = new Guild(g)
+    document.getElementById('guilds').append(guild.Element)
+  }
+  setInterval(refresh, 5000)
 })()
